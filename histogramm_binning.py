Updates - 18.02.2025 Fixed bug in score calculation of histogramm binning algo Added feature to evaluate samples with multiple completitions

histogramm_binning.py
@@ -12,7 +12,7 @@ from tabulate import tabulate
 BASE_DIR    = "/data/stud/2025-MA-kuschnereit/masterarbeit/"
 CHART_DIR = BASE_DIR+"charts_multipl_e/"
 
-binning_type = 'linear'
+binning_type = 'quantil'
 binning_step_size = 0.1
 
 DEBUG = False
@@ -33,12 +33,16 @@ def main():
 
     for d in run_dirs:
         # if main dir is in list just continue
-        if d == args.dirs[0]:
+        if d == args.dirs[0] and ("humaneval" not in d and "mbpp" not in d):
             continue
         run_entry = []
         # Get run name
         run = d.split("/runs/", 1)[1]
         run_entry.append(run)
+
+        # create directory for chart generation
+        if not os.path.isdir(CHART_DIR+run):
+            os.makedirs(CHART_DIR+run)
 
         # load the data from the run directory
         run = run.replace('/', '')
@@ -56,7 +60,7 @@ def main():
             grid = binning.create_unform_grid(10)   
         elif binning_type == 'quantil':
             # get quantils for step size n
-            bin_edges = binning.create_qunatil_grid(binning_step_size)
+            bin_edges = binning.create_qunatil_grid(probs, binning_step_size)
             # get the middle of the bins for hb
             grid = np.array(((bin_edges[1:]-bin_edges[:-1])/2)+bin_edges[:-1]) 
        
@@ -66,7 +70,7 @@ def main():
         hb = hb_calibration(grid, OUTPUTS, DEBUG)
 
         # Split in train and test
-        train_probs, test_probs, train_y, test_y = train_test_split(probs, is_correct, test_size=0.2)
+        train_probs, test_probs, train_y, test_y = train_test_split(probs, is_correct, test_size=0.33, random_state=42)
         if DEBUG: print(f"Training values: {train_probs}")
         if DEBUG: print(f"Test values: {test_probs}")
 
@@ -88,14 +92,16 @@ def main():
 
         if binning_type == 'linear':
             chart_range = np.arange(0, 1.1, 0.1)
+        elif binning_type == 'quantil':
+            chart_range = grid
 
         create_charts.calibration_comparision_chart(chart_range, f_dach, fit_correct_per_bin, CHART_DIR+run+"/histogramm_binning_calibration_chart_"+binning_type+".png", run)
         table_print.append(run_entry)
         
-    table_print = tabulate(table_print, headers=['Run', 'Num_samples', 'ECE_diff', 'brier_diff', 'brier_ref_diff', 'skill_score_diff', 'ECE_train', 'brier_train', 'brier_ref_train', 'skill_score_train'], tablefmt='orgtbl')
+    table_print = tabulate(table_print, headers=['Run', 'Num_samples', 'ECE_diff', 'MSE_diff', 'brier_ref_diff', 'skill_score_diff', 'ECE_train', 'MSE_train', 'brier_ref_train', 'skill_score_train'], tablefmt='orgtbl')
     print(table_print)
 
-    with open('results/histogramm_binning_results.txt', 'w') as f:
+    with open('results/histogramm_binning_'+binning_type+'_results.txt', 'w') as f:
         f.write(table_print)
 
 if __name__ == "__main__":

tools/data.py
@@ -30,33 +30,42 @@ def for_file(path: Path):
 
     if data is None:
         return None
+    
+    return_values = []
     n = len(data["results"])
-    token_infos         = data["tokens_info"][0]
-    cumulative_logprob  = token_infos["cumulative_logprob"]
-    token_logprobs      = token_infos["token_logprobs"]
-    token_ids           = token_infos["len"]
-    c = len([True for r in data["results"] if r["status"]
-            == "OK" and r["exit_code"] == 0])
-    return {
-        "name": data["name"], 
-        "n": n,
-        "c": c,
-        "temperature": data["temperature"] if "temperature" in data else 0.2,
-        "top_p": data["top_p"],
-        "cumulative_logprob": cumulative_logprob,
-        "token_ids": token_ids,
-        "token_logprobs": token_logprobs
-    }
+    for d, res in zip(data["tokens_info"], data["results"]):
+        token_infos         = d
+        cumulative_logprob  = token_infos["cumulative_logprob"]
+        token_logprobs      = token_infos["token_logprobs"]
+        token_ids           = token_infos["len"]
+        c = 1 if res["status"] == "OK" and res["exit_code"] == 0 else 0
+        
+        res_dict = {
+            "name": data["name"], 
+            "n": n,
+            "c": c,
+            "temperature": data["temperature"] if "temperature" in data else 0.2,
+            "top_p": data["top_p"],
+            "cumulative_logprob": cumulative_logprob,
+            "token_ids": token_ids,
+            "token_logprobs": token_logprobs
+        }
+
+        return_values.append(res_dict)
+
+    return return_values
 
 def load_multipl_e_run(path):
     # load the data from the run directory
     results = [for_file(p) for p in itertools.chain(
                 Path(path).glob("*.results.json"), Path(path).glob("*.results.json.gz"))]
     results = [r for r in results if r is not None]
-    temperature = list(set(r["temperature"] for r in results))[0]
-    top_p = list(set(r["top_p"] for r in results))[0]
 
-    num_samples = len(results)
+    temperature = list(set(r[0]["temperature"] for r in results))[0]
+    top_p = list(set(r[0]["top_p"] for r in results))[0]
+    n = list(set(r[0]["n"] for r in results))[0]
+
+    num_samples = len(results) * n
 
     return results, temperature, top_p, num_samples
 
@@ -66,17 +75,18 @@ def proability_and_correctness_for_samples(results, type="avg_logprob"):
 
     # Get the token probailities from the samples and create arrays
     for r in results:
-        token_count = len(r["token_ids"])
-        cumulative_logprob = r["cumulative_logprob"]
-        # for later use
-        logprobs = [list(logprob.values())[0][0] for logprob in r["token_logprobs"]]
+        for sample in r:
+            token_count = len(sample["token_ids"])
+            cumulative_logprob = sample["cumulative_logprob"]
+            # for later use
+            logprobs = [list(logprob.values())[0][0] for logprob in sample["token_logprobs"]]
 
-        if type == "avg_logprob":
-            prob = np.round(np.exp(cumulative_logprob / token_count), 2) 
-            
-        # collect average token probabilty and correctnes value
-        prob_value_list.append(prob)
-        is_correct.append(1) if r["c"] == 1 else is_correct.append(0)
+            if type == "avg_logprob":
+                prob = np.round(np.exp(cumulative_logprob / token_count), 2) 
+                
+            # collect average token probabilty and correctnes value
+            prob_value_list.append(prob)
+            is_correct.append(1) if sample["c"] == 1 else is_correct.append(0)
 
     prob_value_list     = np.array(prob_value_list)
     is_correct          = np.array(is_correct)

tools/hb_calibration.py
@@ -81,9 +81,10 @@ class hb_calibration:
 
         if self.debug: print(f"TEST Correct per bin: {correct_per_bin}")
 
-        scores = self.calculate_scores(False, X, y, correct_per_bin, correct_count, num_samples, total_per_bin, average_bin_confidence)
-
         f_dach = np.clip(correct_per_bin + self.delta_p_f_, 0, 1)
+
+        scores = self.calculate_scores(False, X, y, f_dach, correct_count, num_samples, total_per_bin, average_bin_confidence)
+
         if self.debug: print(f"TEST Corrected Values: {f_dach}")
 
         return f_dach, scores