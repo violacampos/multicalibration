fa61d8067bf44ccf65d6f34b2fc7b2db9933bc61
Robin Ku <robin.kuschnereit@student.hs-rm.de>
2025-05-13 22:35:33 +0200

    Adjusted sample viewer Added Evaluation results Changed data to dataframe

--- analyse/sample_viewer.py
+++ analyse/sample_viewer.py
@@ -3,69 +3,173 @@ import pickle
 import pandas as pd
 import numpy as np
 from colour import Color
+import json
+from pathlib import Path
+from typing import Optional
+import gzip
+
+def gunzip_json(path: Path) -> Optional[dict]:
+    """
+    Reads a .json.gz file, but produces None if any error occurs.
+    """
+    try:
+        with gzip.open(path, "rt") as f:
+            return json.load(f)
+    except Exception as e:
+        return None
+
+def gzip_json(path: Path, data: dict) -> None:
+    with gzip.open(path, "wt") as f:
+        json.dump(data, f)
+
+def for_file(path: Path):
+    if path.suffix == ".gz":
+        data = gunzip_json(path)
+    else:
+        with open(path, 'r') as f:
+            data = json.load(f)
+
+    if data is None:
+        return None
+    
+    return_values = []
+    n = len(data["results"])
+    for d, res in zip(data["tokens_info"], data["results"]):
+        token_infos         = d
+        cumulative_logprob  = token_infos["cumulative_logprob"]
+        token_logprobs      = token_infos["token_logprobs"]
+        token_ids           = token_infos["len"]
+        c = 1 if res["status"] == "OK" and res["exit_code"] == 0 else 0
+        
+        res_dict = {
+            "name": data["name"], 
+            "language": data["language"],
+            "prompt": data["prompt"],
+            "program": res["program"],
+            "n": n,
+            "c": c,
+            "temperature": data["temperature"] if "temperature" in data else 0.2,
+            "top_p": data["top_p"],
+            "cumulative_logprob": cumulative_logprob,
+            "token_ids": token_ids,
+            "token_logprobs": token_logprobs,
+            "status": res["status"]
+        }
+
+        return_values.append(res_dict)
+
+    return return_values
 
 st.set_page_config(layout="wide")
-run = 'humaneval-all-keep-Qwen2.5_Coder_14B-Instruct-1.0-comp-1'
-binning_type = 'linear'
 
-with open('./calibration_data/'+run+'/'+binning_type+'/calibration.pkl', 'rb') as f:
+run = 'humaneval-all-keep-Qwen2.5_Coder_7B-Instruct-1.0-comp-1'
+method = 'comparison'
+binning_type = 'linear'
+prob_method = 'avg_logprob'
+split = 'all'
+model = 'gpt_4o_mini'
+
+with open('./runs/'+run+'/'+method+'/'+binning_type+'/'+prob_method+'/'+split+'/calibration_data/calibration.pkl', 'rb') as f:
     data = pickle.load(f)
 
+with open('./runs/'+run+'/'+method+'/'+binning_type+'/qualitativ/'+split+'/'+model+'/calibration_data/calibration.pkl', 'rb') as f:
+    data_quant = pickle.load(f)
+
+with open('./runs/'+run+'/'+method+'/'+binning_type+'/quantitativ/'+split+'/'+model+'/calibration_data/calibration.pkl', 'rb') as f:
+    data_qual = pickle.load(f)
 
 st.title("Sample viewer")
 
+correctness = st.toggle("Correct (ON -> all correct, OFF -> all incorrect)")
+
+df = pd.DataFrame({'names': data["names"], 
+                   'language': data["language"], 
+                   'is_correct': data["is_correct"],
+                   'uncalib_avg_token_probs': data["uncalibrated_probs"],
+                   'uncalib_qual_probs': data_qual["uncalibrated_probs"],
+                   'uncalib_quant_probs': data_quant["uncalibrated_probs"],
+                   'hb_probs': data["calibrated_probs_hb"],
+                   'lr_probs': data["calibrated_probs_lr"],
+                   'ighb_probs': data["calibrated_probs_ighb"],
+                   'iglb_probs': data["calibrated_probs_iglb"],
+                   'groups': data["groups"].tolist(),
+                   'prompts': data["prompts"],
+                   'token_logprobs': data["token_logprobs"],
+                   'programs': data["programs"]})
+
+if correctness:
+    df = df[df["is_correct"] == 1]
+else:
+    df = df[df["is_correct"] == 0]   
+
+st.write(f"Total: {len(df)}")
+
 selection = [s1 + '_' + s2 + '_' + str(s3) for s1, s2, s3 in zip(data["language"], data["names"], data["is_correct"])]
 
-option = st.selectbox(
-                        "**Name:**",
-                        selection,
+sorted_lang = list(dict.fromkeys(df["language"]))
+sorted_lang.sort()
+
+lang = st.selectbox(
+                        "**Language:**",
+                        sorted_lang,
                     )
 
-item_id = selection.index(option)
+df = df[df["language"] == lang]
 
-st.write("**Name:**", data["names"][item_id])
-st.write("**Language:** ", data["language"][item_id])
+name = st.selectbox(
+                        "**Name:**",
+                        df["names"],
+                    )
+
+df = df[df["names"] == name]
+p = Path('../MultiPL-E/runs/'+run+'/'+lang+'/'+name+'.results.json.gz')
+
+multiple_res = for_file(p)
 
 
-if data["is_correct"][item_id] == 1:
+if df["is_correct"].values[0] == 1:
     st.markdown("**Evaluation:** :green-badge[:material/check: Correct]")
 else:
     st.markdown("**Evaluation:** :red-badge[:material/close: Inorrect]")
 
-st.write("**Probailities**:")
-df = pd.DataFrame(
+st.write(f'**Evaluation result:** {multiple_res[0]["status"]}')
+
+st.write("**Probailities:**")
+prob_df = pd.DataFrame(
     {
-        "Uncalib": [data["uncalibrated_probs"][item_id]],
-        "HB": [np.round(data["calibrated_probs_hb"][item_id], 2)],
-        "LR": [np.round(data["calibrated_probs_lr"][item_id], 2)],
-        "IGHB": [np.round(data["calibrated_probs_ighb"][item_id], 2)],
-        "IGLB": [np.round(data["calibrated_probs_iglb"][item_id], 2)],
+        "Uncalib (avg)": [df["uncalib_avg_token_probs"].values[0]],
+        "Uncalib (qual)": [df["uncalib_qual_probs"].values[0]],
+        "Uncalib (quant)": [df["uncalib_quant_probs"].values[0]],
+        "HB": [np.round(df["hb_probs"].values[0], 2)],
+        "LR": [np.round(df["lr_probs"].values[0], 2)],
+        "IGHB": [np.round(df["ighb_probs"].values[0], 2)],
+        "IGLB": [np.round(df["iglb_probs"].values[0], 2)],
     }
 )
-st.dataframe(df.style.format("{:.0%}"), hide_index=True)
+st.dataframe(prob_df.style.format("{:.0%}"), hide_index=True)
 
-st.write("**Groups**:")
-groups = pd.DataFrame([data["groups"][item_id]])
+st.write("**Groups:**")
+groups = pd.DataFrame([df["groups"].values[0]])
 groups.columns = ["simple complexity", "more complex", "complex", "untestable", "prompt >= 500", "not >= 500", "has_examples", "not example", "Longer then median loc", "not loc"]
 st.dataframe(groups, hide_index=True)
 
 red = Color("red")
 colors = list(red.range_to(Color("green"),100))
 with st.expander("See prompt"):
-    st.write("**Prompt**:")
-    st.code(data["prompts"][item_id])
+    st.write("**Prompt:**")
+    st.code(df["prompts"].values[0])
 
 output = ''
-for token in data["token_logprobs"][item_id]:
+for token in df["token_logprobs"].values[0]:
     tv = list(token.values())[0]
     prob = np.round(np.exp(tv[0]),2)
     output += '<span style="color:'+str(colors[int(prob*100)-1])+'">'+tv[2].replace("Ġ", " ").replace("Ċ", "\n")+'</span>'
 
 with st.expander("See higlighted code"):
-    st.write("**Generated code**:")
+    st.write("**Generated code:**")
     st.html('<pre style="background-color: #EBECE4">'+output+'</pre>')
 
-st.write("**Evaluated program**:")
-st.code(data["programs"][item_id])
+st.write("**Evaluated program:**")
+st.code(df["programs"].values[0])
 
 
